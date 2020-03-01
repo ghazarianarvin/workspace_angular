174a53020235ba2ab62ab9a963f24c6eacab7bbf
ghazarian.as@gmail.com <ghazarian.as@gmail.com>
2020-03-01 18:53:19 +0100

    second outlet works!

--- an-routing/src/app/app.module.ts
+++ an-routing/src/app/app.module.ts
@@ -4,28 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ComponentOneComponent } from './component-one/component-one.component';
 import { ComponentTwoComponent } from './component-two/component-two.component';
-import {RouterModule, Routes} from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule, Routes } from '@angular/router';
 import {FormsModule} from '@angular/forms';
 
 const routes: Routes = [
-  { path: '', component: ComponentOneComponent},
   { path: 'one', component: ComponentOneComponent},
+  { path: 'oneOnTop', component: ComponentOneComponent, outlet: 'topOutlet'},
   { path: 'two/:param', component: ComponentTwoComponent},
+  { path: 'twoOnTop/:param', component: ComponentTwoComponent, outlet: 'topOutlet'},
   { path: 'two', component: ComponentTwoComponent},
-  { path: '**', component: PageNotFoundComponent }
+  { path: 'twoOnTop', component: ComponentTwoComponent, outlet: 'topOutlet'},
+
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentOneComponent,
-    ComponentTwoComponent,
-    PageNotFoundComponent
+    ComponentTwoComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { enableTracing: true }),
     FormsModule
   ],
   providers: [],
