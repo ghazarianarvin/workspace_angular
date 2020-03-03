820fcd1b1f0a5f182d7dca7497248e4bbfbe6bd8
ghazarian.as@gmail.com <ghazarian.as@gmail.com>
2020-03-03 23:24:15 +0100

    detail router outlet works

---
+++ an-ionic/src/app/dog-information/dog-detail/dog-detail/dog-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-detail',
+  templateUrl: './dog-detail.component.html',
+  styleUrls: ['./dog-detail.component.scss'],
+})
+export class DogDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
